d921d5d641212e06913740c493ea0068bad3581f
Khiem Pham <khiempham841@gmail.com>
2021-11-12 18:11:46 -0800

    implemented create recipe page

--- source/client/assets/scripts/main.js
+++ source/client/assets/scripts/main.js
@@ -1,87 +1,137 @@
-import { Router } from "./Router.js";
+import { Router } from './Router.js'
 
-const recipes = [];
-const recipeData = {};
+const recipes = []
+const recipeData = {}
 
 const router = new Router(function () {
+  document.querySelector('section.section--recipe-cards').classList.add('shown')
   document
-    .querySelector("section.section--recipe-cards")
-    .classList.add("shown");
-  document
-    .querySelector("section.section--recipe-expand")
-    .classList.remove("shown");
-});
+    .querySelector('section.section--recipe-expand')
+    .classList.remove('shown')
+})
 
-window.addEventListener("DOMContentLoaded", init);
+window.addEventListener('DOMContentLoaded', init)
 
-async function init() {
+async function init () {
   try {
-    await fetchRecipes();
+    await fetchRecipes()
   } catch (err) {
-    console.log(`Error fetching recipes: ${err}`);
-    return;
+    console.log(`Error fetching recipes: ${err}`)
+    return
   }
 
-  createRecipeCards();
-  bindPopstate();
+  createRecipeCards()
+  bindPopstate()
 }
 
-async function fetchRecipes() {
+async function fetchRecipes () {
   return new Promise((resolve, reject) => {
-    recipes.forEach((recipe) => {
+    recipes.forEach(recipe => {
       fetch(recipe)
-        .then((response) => response.json())
-        .then((data) => {
+        .then(response => response.json())
+        .then(data => {
           // This grabs the page name from the URL in the array above
-          data["page-name"] = recipe.split("/").pop().split(".")[0];
-          recipeData[recipe] = data;
+          data['page-name'] = recipe
+            .split('/')
+            .pop()
+            .split('.')[0]
+          recipeData[recipe] = data
           if (Object.keys(recipeData).length == recipes.length) {
-            resolve();
+            resolve()
           }
         })
-        .catch((err) => {
-          console.log(`Error loading the ${recipe} recipe`);
-          reject(err);
-        });
-    });
-  });
+        .catch(err => {
+          console.log(`Error loading the ${recipe} recipe`)
+          reject(err)
+        })
+    })
+  })
 }
 
-function createRecipeCards() {
+function createRecipeCards () {
   recipes.forEach((element, i) => {
-    const recipeCard = document.createElement("recipe-card");
+    const recipeCard = document.createElement('recipe-card')
 
-    recipeCard.data = recipeData[element];
+    recipeCard.data = recipeData[element]
 
-    const page = recipeData[element]["page-name"];
+    const page = recipeData[element]['page-name']
     router.addPage(page, function () {
-      document
-        .querySelector(".section--recipe-cards")
-        .classList.remove("shown");
-      document.querySelector(".section--recipe-expand").classList.add("shown");
-      document.querySelector("recipe-expand").data = recipeData[element];
-    });
-    console.log(recipeCard);
-    bindRecipeCard(recipeCard, page);
+      document.querySelector('.section--recipe-cards').classList.remove('shown')
+      document.querySelector('.section--recipe-expand').classList.add('shown')
+      document.querySelector('recipe-expand').data = recipeData[element]
+    })
+    console.log(recipeCard)
+    bindRecipeCard(recipeCard, page)
 
-    document.querySelector(".recipe-cards--wrapper").appendChild(recipeCard);
-  });
+    document.querySelector('.recipe-cards--wrapper').appendChild(recipeCard)
+  })
 }
 
-function bindRecipeCard(recipeCard, pageName) {
-  recipeCard.addEventListener("click", (e) => {
-    if (e.path[0].nodeName == "A") return;
-    router.navigate(pageName);
-  });
+function bindRecipeCard (recipeCard, pageName) {
+  recipeCard.addEventListener('click', e => {
+    if (e.path[0].nodeName == 'A') return
+    router.navigate(pageName)
+  })
 }
 
-function bindPopstate() {
-  window.addEventListener("popstate", (e) => {
-    console.log(e.state);
+function bindPopstate () {
+  window.addEventListener('popstate', e => {
+    console.log(e.state)
     if (e.state) {
-      router.navigate(e.state.page, true);
+      router.navigate(e.state.page, true)
     } else {
-      router.navigate("home", true);
+      router.navigate('home', true)
     }
-  });
+  })
+}
+
+// CREATE RECIPE PAGE FUNCTIONS
+
+/*
+// Get image preview element
+let preview = document.getElementById('display-image')
+
+// Convert image file to string
+document.getElementById('input-img').addEventListener('change', e => {
+  const file = e.target.files[0]
+  const reader = new FileReader()
+  reader.onloadend = () => {
+    // convert file to base64 String
+    const base64String = reader.result.replace('data:', '').replace(/^.+,/, '')
+    // Set img preview source to file image
+    preview.setAttribute('src', `data:image/png;base64,${base64String}`)
+  }
+  reader.readAsDataURL(file)
+})
+*/
+
+/*
+ * Function to get image and display it on preview
+ */
+/*
+function handleImageUpload () {
+  let image = document.getElementById('upload').files[0]
+  let fileReader = new FileReader()
+  fileReader.onload = function (e) {
+    document.getElementById('display-image').src = e.target.result
+  }
+  if (image) {
+    fileReader.readAsDataURL(image)
+  }
+}
+*/
+
+function hello () {
+  alert('hello')
+}
+
+function handleImageUpload () {
+  let image = document.getElementById('input-img').files[0]
+  let fileReader = new FileReader()
+  fileReader.onload = function (e) {
+    document.getElementById('display-image').src = e.target.result
+  }
+  if (image) {
+    fileReader.readAsDataURL(image)
+  }
 }
