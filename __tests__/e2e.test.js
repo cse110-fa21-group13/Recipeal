cb39d4778780f57c643fb4e8fb11659bed94b9c9
Jason Win <jasonwin2000@gmail.com>
2021-11-15 19:40:42 -0800

    fix sample test again

--- __tests__/e2e.test.js
+++ __tests__/e2e.test.js
@@ -8,10 +8,13 @@ describe('Basic user flow for Website', () => {
     it('Initial Home Page - Check if is on the right page', async () => {
         console.log('Checking my recipes page');
 
-        const pages = await page.$$('section');
-        const homePage = await pages[1].getProperty('innerHTML');
-        const homePageText = homePage['_remoteObject'].value;
+        const buttons = await page.$$('button');
+        const myRecipeBtn = await buttons[0].getProperty('innerHTML');
+        const exploreBtn = await buttons[1].getProperty('innerHTML');
+        const text1 = myRecipeBtn['_remoteObject'].value;
+        const text2 = exploreBtn['_remoteObject'].value;
 
-        expect(homePageText).toContain('My recipes');
+        expect(text1).toContain('My Recipes');
+        expect(text2).toContain('Explore');
       });
   });
