19d5559aed50375bbee2b8a92ff8ebbe43aff5f4
brett <brett.beattie99@gmail.com>
2021-11-16 12:47:37 -0800

    updated recipe card styling to look like prototype

--- source/client/assets/components/recipe-card.js
+++ source/client/assets/components/recipe-card.js
@@ -1,77 +1,98 @@
 class RecipeCard extends HTMLElement {
   constructor () {
     super(); // Inheret everything from HTMLElement
-
     // Attach the shadow DOM and append this markup / stlying inside
     // The shadow root will help us keep everything separated
     let shadow = this.attachShadow({ mode: 'open' });
+    shadow.innerHTML = `<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">`;
   }
 
   set data (data) {
     // if (!data) return;
-    const styleElem = document.createElement('style');
-    const styles = `
-      * {
-        font-family: sans-serif;
-        margin: 0;
-        padding: 0;
-      }
+    const styles = document.createElement('style');
+    styles.innerHTML = `
       article {
-        align-items: center;
-        border: 1px solid rgb(223, 225, 229);
-        border-radius: 8px;
-        display: grid;
-        grid-template-rows: 118px 56px 14px 18px 15px 36px;
-        height: auto;
-        row-gap: 5px;
-        padding: 0 16px 16px 16px;
-        width: 178px;
+        display: flexbox;
+        position: relative;
+        font-family: 'Work Sans', sans-serif;
+        width: 200px;
+        height: 325px;
+        box-shadow: 0 4px 10px rgba(0,0,0,0.4);
+        border-radius: 16px;
+        transition: all ease-out 0.3s;
       }
-      
-      article > img {
-        border-top-left-radius: 8px;
-        border-top-right-radius: 8px;
-        height: 118px;
-        object-fit: cover;
-        margin-left: -16px;
-        width: calc(100% + 32px);
+
+      article:hover {
+        box-shadow: 0 6px 25px rgba(0,0,0,0.3);
+        cursor: pointer;
       }
-      p.card-description {
-        height: 32px;
-        line-height: 16px;
-        padding-top: 4px;
-        overflow: hidden;
+
+      .image-box {
+        width: 200px;
+        height: 200px;
       }
-      p.card-title {
-        display: -webkit-box;
-        font-size: 16px;
-        height: 36px;
-        line-height: 18px;
-        overflow: hidden;
-        -webkit-line-clamp: 2;
-        -webkit-box-orient: vertical;
+
+      .image-box img, .image-box h1{
+          margin: 0;
+          width: 100%;
+          height: 100%;
+          object-fit: cover;
+          border-radius: 16px 16px 0 0;
       }
-      p:not(.title), span, time {
-        color: #70757A;
-        font-size: 12px;
+
+      .image-box h1 {
+          width: auto;
+          padding: 0px 9px;
+      }
+
+      .image-box .card-title {
+          position: absolute;
+          margin: 0;
+          top: 120px;
+          font-size: 16px;
+          font-weight: bolder;
+          color: white;
+          text-shadow: 0 2px 4px rgba(0,0,0,0.7);
+      }
+
+      .textbox {
+          border-top: solid 4px #FFC148;
+          height: 80px;
+          position: relative;
+          vertical-align: middle;
+          overflow: hidden;
+      }
+
+      .textbox p{
+          position: absolute;
+          top: 30px;
+          transform: translateY(-50%);
+          font-size: 16px;
+          margin: 0;
+          padding: 12px 9px;
       }
     `;
-    styleElem.innerHTML = styles;
 
     const card = document.createElement('article');
     card.className = 'card';
 
+    const imageBox = document.createElement('div');
+    imageBox.setAttribute('class', 'image-box');
     const image = document.createElement('img');
-    image.setAttribute('class', 'card-img-top');
     image.setAttribute('src', data.thumbnail);
-
-    const title = document.createElement('p');
-    title.setAttribute('class', 'card-title');
+    const cardTitle = document.createElement('div');
+    cardTitle.setAttribute('class', 'card-title');
+    const title = document.createElement('h1');
     title.textContent = data.name;
+    cardTitle.appendChild(title);
+    imageBox.appendChild(image);
+    imageBox.appendChild(cardTitle);
 
+    const desBox = document.createElement('div');
+    desBox.setAttribute('class', 'textbox');
     const description = document.createElement('p');
-    description.setAttribute('class', 'card-description');
     description.textContent = data.description;
+    desBox.appendChild(description);
 
     const time = document.createElement('time');
 
@@ -89,13 +110,12 @@ class RecipeCard extends HTMLElement {
       time.textContent += `${data.time.minutes} minutes`;
     }
 
-    card.appendChild(image);
-    card.appendChild(title);
-    card.appendChild(description);
-    card.appendChild(time);
 
-    this.shadowRoot.appendChild(card);
-    this.shadowRoot.appendChild(styleElem);
+    card.appendChild(imageBox);
+    card.appendChild(desBox);
+    //card.appendChild(time);
+
+    this.shadowRoot.append(styles, card);
   }
 }
 customElements.define('recipe-card', RecipeCard);
