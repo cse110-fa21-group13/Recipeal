056e5aada4411c3959d4d39957aeb1ffd0121bde
brett <brett.beattie99@gmail.com>
2021-11-22 13:22:53 -0800

    Changing imports again

--- source/client/assets/scripts/create-recipe.js
+++ source/client/assets/scripts/create-recipe.js
@@ -1,7 +1,7 @@
 // Contains the functions involved in creating and editing a new recipe
 
-import { newCard } from "/source/client/assets/scripts/recipe-card-create.js";
-import { changeView, switchButtonView } from "/source/client/assets/scripts/navigate.js";
+import { newCard } from "/assets/scripts/recipe-card-create.js";
+import { changeView, switchButtonView } from "/assets/scripts/navigate.js";
 
 /** ELEMENTS **/
 
--- source/client/assets/scripts/recipe-card-create.js
+++ source/client/assets/scripts/recipe-card-create.js
@@ -1,4 +1,4 @@
-import { changeView } from "/source/client/assets/scripts/navigate.js";
+import { changeView } from "/assets/scripts/navigate.js";
 
 /**
  * @method newCard
