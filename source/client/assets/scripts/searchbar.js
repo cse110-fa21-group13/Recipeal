8a5bf4963bc8152770d23f2a3ea4935fbb04229a
bananasnowcones <43969773+bananasnowcones@users.noreply.github.com>
2021-11-28 19:58:52 -0800

    started nav on explore

--- source/client/assets/scripts/searchbar.js
+++ source/client/assets/scripts/searchbar.js
@@ -1,6 +1,6 @@
 /* 
-* Function to search for recipe cards in search bar
-*/
+ * Function to search for recipe cards in search bar
+ */
 function searchRecipe() {
     var input = document.getElementById("search-bar");
     var filter = input.value.toLowerCase();
@@ -16,5 +16,36 @@ function searchRecipe() {
         } else {
         currentRecipe.style.display = "none"; 
         }
+    }
+}
+/* 
+ * TODO: 
+ * Function to search for recipes in Spoonacular
+ */
+function searchSpoon() {
+    var input = document.getElementById("explore-search-bar");
+    const filter = input.value.toLowerCase();
+    var recipeCardsElement = document.getElementById('explore-wrapper');
+    var allRecipes = recipeCardsElement.children; 
+    if (filter === "") {
+        for (let i = 0; i < allRecipes.length; i++) {
+            let currentRecipe = allRecipes[i];
+            currentRecipe.style.display = "";
+        }
+    }
+    else {
+        for (let i = 0; i < allRecipes.length; i++) {
+            let currentRecipe = allRecipes[i]; 
+            currentRecipe.style.display = "none"; 
+        }
+        const SEARCH_URL = `https://api.spoonacular.com/recipes/complexSearch?query=titleMatch=${filter}`;
+        fetch (SEARCH_URL)
+            .then(response => response.json())
+            .then(data => {
+                for (let i = 0; i < data.results.length; i++) {
+                    
+                }
+            });
+
     }
 }
