ba19bf3efeb4593245fcb47b61ba983f600bfa51
songyu lu <songyulu99@gmail.com>
2021-12-01 14:46:49 -0800

    workflow

--- cypress-testing/cypress/integration/my-own-test/connect.js
+++ cypress-testing/cypress/integration/my-own-test/connect.js
@@ -10,7 +10,28 @@ describe('example to-do app', () => {
       cy.visit('https://project-cupcake.netlify.app/')
     })
 
-    it("do something", ()=>{
-        cy.log('something');
+    it("search before create", ()=>{
+      cy.get('input#search-bar')          
+      .then($els => {
+        expect($els.length).to.eq(1)    
+      })
+      .type('myfood');
+      cy.get('button#filter-btn').click();
+      cy.get('div#recipe-cards').
+        then($els => {
+        expect($els.length).to.eq(1)    
+      })
+    })
+
+    it('"search after single copy', ()=>{
+      cy.get('button#create-recipe-btn').click();
+      cy.get('input#input-name').type('myfood');
+      cy.get('textarea#input-desc').type('this is some desc');
+      cy.get('input#input-hours').type('1');
+      cy.get('input#input-mins').type('30');
+      cy.get('input#input-tags1').type('1st tag');
+      cy.get('input#input-ings1').type('1st ings');
+      cy.get('input#input-steps1').type('1st steps');
+      cy.xpath('//*[@id="create-recipe--input-wrapper"]/div[3]/button[1]').click();
     })
 })
