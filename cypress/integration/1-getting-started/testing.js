d5a6462e655497aeabcd424a7e2f1bb34a4588f7
songyu lu <songyulu99@gmail.com>
2021-12-10 13:41:53 -0800

    curd test

--- cypress/integration/1-getting-started/testing.js
+++ cypress/integration/1-getting-started/testing.js
@@ -24,6 +24,7 @@ describe('example to-do app', () => {
     });
   });
   */
+  
 
   
   it('create', ()=>{
@@ -152,6 +153,78 @@ describe('create one sample recipe', ()=>{
 
 
 
+describe('delete recipe', ()=>{
+  beforeEach(() => {
+    cy.visit('https://recipeal.netlify.app/');
+
+    cy.xpath('/html/body/div/div/div[6]/button[2]').click();
+    cy.get('button#create-recipe-btn').click();
+    cy.wait(500);
+    
+    cy.get('input#input-name').type('myfood');
+    cy.get('textarea#input-desc').type('this is some desc');
+    cy.get('input#input-hours').type('1');
+    cy.get('input#input-mins').type('30');
+    cy.xpath('//*[@id="ing-card"]/button').click();
+    cy.get('input#input-ings1').type('1st ings');
+    cy.xpath('//*[@id="step-card"]/button').click();
+    cy.get('input#input-steps1').type('1st steps');
+    cy.xpath('//*[@id="create-recipe--input-wrapper"]/div[3]/button[1]').click();
+    cy.get('button#return-btn').click();
+    // check if element is being saved
+    cy.get('recipe-card#myfood').should('exist');
+
+    cy.get('button#create-recipe-btn').click();
+    cy.get('input#input-name').type('myfood-delete');
+    cy.get('textarea#input-desc').type('this is some desc');
+    cy.get('input#input-hours').type('1');
+    cy.get('input#input-mins').type('30');
+    cy.xpath('//*[@id="ing-card"]/button').click();
+    cy.get('input#input-ings1').type('1st ings');
+    cy.xpath('//*[@id="step-card"]/button').click();
+    cy.get('input#input-steps1').type('1st steps');
+    cy.xpath('//*[@id="create-recipe--input-wrapper"]/div[3]/button[1]').click();
+    cy.get('button#return-btn').click();
+    cy.get('recipe-card#myfood-delete').should('exist');
+  });
+
+  it('delete first', ()=>{
+    cy.xpath('//*[@id="delete-btn"]').click();
+    cy.get('recipe-card#myfood') 
+      .shadow() 
+       .find('article')
+          .find('button#cardDelete') 
+            .click()
+  });
+
+  it('delete second', ()=>{
+    cy.xpath('//*[@id="delete-btn"]').click();
+    cy.get('recipe-card#myfood-delete') 
+            .shadow() 
+            .find('article')
+            .find('button#cardDelete') 
+            .click()
+  });
+  /*
+  it('change name', ()=>{
+
+  })
+
+  it('change name', ()=>{
+
+  })
+
+  it('change name', ()=>{
+
+  })
+
+  it('change name', ()=>{
+
+  })*/
+
+});
+
+
 describe('update recipe', ()=>{
   beforeEach(() => {
     cy.visit('https://recipeal.netlify.app/');
@@ -187,12 +260,28 @@ describe('update recipe', ()=>{
     cy.get('recipe-card#myfood-delete').should('exist');
   });
 
-  it('delete', ()=>{
-    cy.xpath('//*[@id="delete-btn"]').click();
-    cy.xpath('//*[@id="myfood"]').should('be.visible');
-    //cy.xpath('//*[@id="myfood"]//article').should('be.visible');
-    
-    //cy.xpath('//*[@id="delete-btn"]').click();
+  it('edit title', ()=>{
+    cy.get('recipe-card#myfood').click();
+    cy.xpath('//*[@id="edit-btn"]').click();   
+    cy.get('input#input-name').type('myfood-changed');
+    cy.xpath('//*[@id="save-edit-btn"]').click();
+  });
+
+  it('edit desc', ()=>{
+    cy.get('recipe-card#myfood-delete').click();
+    cy.xpath('//*[@id="edit-btn"]').click();
+    cy.get('textarea#input-desc').type('change some des for recipe 2');
+    cy.xpath('//*[@id="save-edit-btn"]').click();
+  });
+
+  it('edit time', ()=>{
+    cy.get('recipe-card#myfood-delete').click();
+    cy.xpath('//*[@id="edit-btn"]').click();
+    cy.get('input#input-hours').clear();
+    cy.get('input#input-hours').type('10');
+    cy.get('input#input-mins').clear();
+    cy.get('input#input-mins').type('20');
+    cy.xpath('//*[@id="save-edit-btn"]').click();
   });
   /*
   it('change name', ()=>{
@@ -213,7 +302,7 @@ describe('update recipe', ()=>{
 
 });
 
-describe('update recipe', ()=>{
+describe('filter recipe', ()=>{
   beforeEach(() => {
     cy.visit('https://recipeal.netlify.app/');
 
@@ -289,7 +378,8 @@ describe('update recipe', ()=>{
   });
 
   it("4 recipes", ()=>{
-
+    cy.get('recipe-card#myfood').click();
+    cy.xpath('//*[@id="edit-btn"]').click();
   })
 
 });
